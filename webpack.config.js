4e4055e1c22e258353bdc1a6e0dc1cf70514a1b6
Mike <ssbeefeater@gmail.com>
2018-04-09 21:27:21 +0300

    fix build and change build directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,23 @@
 module.exports = {
+    entry: {
+        'react-localizer': './src/index.js',
+    },
+    externals: {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+        },
+    },
+    output: {
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js',
+        path: __dirname,
+        publicPath: '/dist',
+        libraryTarget: 'umd',
+        library: 'react-localizer',
+    },
     module: {
         rules: [
             {
@@ -11,3 +30,5 @@ module.exports = {
         ],
     },
 };
+
+
