b4e1c0fba46dd060fa9fc53f404bc44f2a52ba84
Mike <ssbeefeater@gmail.com>
2018-04-11 01:53:42 +0300

    fix the coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    coveragePathIgnorePatterns: ['<rootDir>/__tests__/'],
+    coveragePathIgnorePatterns: ['<rootDir>/__tests__/', '<rootDir>/src/index.js', '<rootDir>/src/localeShape.js', '<rootDir>/src/LocaleContext.js'],
     testRegex: '/__tests__/.*\\.test.(js)$',
 };
